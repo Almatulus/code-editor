0939b4161cba8d0aabcbfb43b146f4be376d2bb5
Almatulus <a.kaipzhanov@gmail.com>
2026-09-06 20:19:07 +0500

    refactor: Декомпозиция AppHeader и AppFooter на мелкие компоненты

--- app/app.vue
+++ app/app.vue
@@ -1,11 +1,3 @@
-<script setup lang="ts">
-import AppFeatures from "./components/AppFeatures.vue";
-import AppFooter from "./components/AppFooter.vue";
-import AppHeader from "./components/AppHeader.vue";
-import AppHero from "./components/AppHero.vue";
-import AppNewsLetter from "./components/AppNewsLetter.vue";
-</script>
-
 <template>
   <div class="dark flex min-h-screen flex-col dark:bg-gray-800">
     <AppHeader />
