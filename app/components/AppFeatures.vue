612a139083037cffd51b6ff0c4c348d6493dda14
Almatulus <a.kaipzhanov@gmail.com>
2026-09-07 20:33:51 +0500

    refactor: Рефакторинг отдельных компонентов шапки секции в отдельный универсальный компонент для заголовка секции AppSectionHeader

--- app/components/AppFeatures.vue
+++ app/components/AppFeatures.vue
@@ -1,9 +1,20 @@
+<script setup lang="ts">
+const sectionHeader = {
+  title: "Unlock the Full Potential",
+  description: ` With tailored settings, keyboard shortcuts, and powerful
+          extensions, you can work smarter and faster.`,
+};
+</script>
+
 <template>
   <section
     class="flex flex-col gap-y-8 bg-gray-50 py-8 sm:gap-y-12 sm:py-16 dark:bg-gray-700"
     id="features"
   >
-    <AppFeaturesHeader />
+    <AppSectionHeader
+      :title="sectionHeader.title"
+      :description="sectionHeader.description"
+    />
     <AppFeaturesContent />
   </section>
 </template>
--- app/components/AppNewsletter.vue
+++ app/components/AppNewsletter.vue
@@ -1,6 +1,17 @@
+<script setup lang="ts">
+const sectionHeader = {
+  title: "Extensions",
+  description: `Expand your capabilities with powerful extensions tailored to enhance your
+              coding experience.`,
+};
+</script>
+
 <template>
   <section class="flex flex-col gap-y-6 py-8 sm:gap-y-8 sm:py-16">
-    <AppNewsletterHeader />
+    <AppSectionHeader
+      :title="sectionHeader.title"
+      :description="sectionHeader.description"
+    />
     <AppNewsletterContent />
   </section>
 </template>
