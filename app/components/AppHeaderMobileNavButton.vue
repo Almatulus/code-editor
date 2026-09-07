149d1537756f45ae90e16f4506d4fe949c6717ba
Almatulus <a.kaipzhanov@gmail.com>
2026-09-07 18:00:13 +0500

    refactor: Декомпозиция ссылок внутри AppHeaderMobileNav на составные компоненты AppHeaderMobileNavButton и AppHeaderMobileNavItem

---
+++ app/components/AppHeaderMobileNavButton.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+defineProps<{ iconName: string }>();
+</script>
+
+<template>
+  <a class="-mr-1.5 rounded-md p-2 hover:bg-gray-100 dark:hover:bg-gray-700">
+    <Icon :name="iconName" class="size-6 dark:text-white" />
+  </a>
+</template>
