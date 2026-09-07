8584540786cb81c0617427f8fa7d62d1594a0a05
Almatulus <a.kaipzhanov@gmail.com>
2026-09-07 17:40:43 +0500

    refactor: Декомпозиция ссылок внутри AppHeaderNav на отдельные компоненты AppHeaderNavItem

---
+++ app/components/AppHeaderNavItem.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+const {
+  linkClasses = "font-semibold hover:text-indigo-600 dark:text-gray-200 dark:hover:text-indigo-200",
+} = defineProps<{ linkClasses?: string }>();
+</script>
+
+<template>
+  <li>
+    <a href="#" :class="linkClasses"> <slot></slot></a>
+  </li>
+</template>
