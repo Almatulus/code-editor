e882870764ddf91a6d85379788444e47fa6e5dc5
Almatulus <a.kaipzhanov@gmail.com>
2026-09-07 21:08:33 +0500

    refactor: Декомпозиция элементов списка меню футера в отдельные компоненты

---
+++ app/components/AppFooterList.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps<{
+  list: {
+    title: string;
+    links: string[];
+  };
+}>();
+</script>
+
+<template>
+  <div class="flex flex-col gap-y-3 sm:gap-y-4">
+    <AppFooterListHeading>{{ list.title }} </AppFooterListHeading>
+    <ul class="flex flex-col gap-y-1 sm:gap-y-2">
+      <AppFooterLinksItem v-for="link in list.links" :key="link">{{
+        link
+      }}</AppFooterLinksItem>
+    </ul>
+  </div>
+</template>
---
+++ app/components/AppFooterLists.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const list = [
+  {
+    title: "Popular Playlists",
+    links: ["Prettier", "GitLens", "Live Share"],
+  },
+  {
+    title: "Configurations",
+    links: ["Settings", "Keyboard Shortcuts", "Code Snippets"],
+  },
+  {
+    title: "Socials",
+    links: ["YouTube", "Telegram", "GitHub"],
+  },
+];
+</script>
+
+<template>
+  <div class="flex flex-col gap-y-8 sm:flex-row sm:gap-x-16">
+    <AppFooterList v-for="item in list" :key="item.title" :list="item" />
+  </div>
+</template>
