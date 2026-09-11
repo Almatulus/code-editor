573d31741d9f5e2b6a7e1ce50fdf8d9071da9f7c
Almatulus <a.kaipzhanov@gmail.com>
2026-09-11 15:43:51 +0500

    31: Рефакторинг компонента логотипа

---
+++ app/components/AppLogo.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{
+  iconClasses: string;
+}>();
+</script>
+
+<template>
+  <NuxtLink to="/" class="flex items-center gap-x-1 font-black sm:gap-x-2">
+    <Icon name="code" :class="`-rotate-6 ${iconClasses}`" />
+
+    <span v-if="$slots.default"> <slot></slot></span>
+  </NuxtLink>
+</template>
