0bbb5984f5d7ba22a23ab2372a48040116323c9c
Almatulus <a.kaipzhanov@gmail.com>
2026-09-21 20:48:20 +0500

    49: Реализация  отправки данных через форму регистрации

--- app/pages/register.vue
+++ app/pages/register.vue
@@ -2,17 +2,47 @@
 definePageMeta({
   layout: "auth",
 });
+
+const form = ref({
+  email: "",
+  password: "",
+  repeatPassword: "",
+});
+
+const { loggedIn, fetch: fetchUserSession } = useUserSession();
+
+if (loggedIn.value) {
+  navigateTo("/playlist");
+}
+
+async function handleRegister() {
+  await $fetch("/api/register", {
+    method: "POST",
+    body: form.value,
+  });
+
+  await fetchUserSession();
+
+  await navigateTo("/playlist");
+}
 </script>
 
 <template>
   <form class="flex flex-col gap-y-4 sm:gap-y-6">
-    <AppFormField type="email" id="email">Email</AppFormField>
-    <AppFormField type="password" id="password">Password</AppFormField>
-    <AppFormField type="password" id="password"
+    {{ loggedIn }}
+    <AppFormField v-model.trim="form.email" type="email" id="email"
+      >Email</AppFormField
+    >
+    <AppFormField v-model="form.password" type="password" id="password"
+      >Password</AppFormField
+    >
+    <AppFormField v-model="form.repeatPassword" type="password" id="password"
       >Password Confirmation</AppFormField
     >
 
-    <AppButton class="mt-2 sm:mt-1"> Sign Up</AppButton>
+    <AppButton @click.prevent="handleRegister" class="mt-2 sm:mt-1">
+      Sign Up</AppButton
+    >
   </form>
   <p class="mt-6 text-center text-sm text-gray-600 sm:mt-8 dark:text-gray-300">
     Already have an account?
