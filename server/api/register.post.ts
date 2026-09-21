2b3fe22dcd209e7736919b5bb4321f01b275c409
Almatulus <a.kaipzhanov@gmail.com>
2026-09-21 20:24:34 +0500

    48: Реализация логики регистрации

--- server/api/register.post.ts
+++ server/api/register.post.ts
@@ -1,15 +1,44 @@
 import { users } from "../database/schema";
 
 export default defineEventHandler(async (event) => {
-  return useDrizzle()
+  const { email, password } = await readBody(event);
+
+  const db = useDrizzle();
+
+  const existingUser = await db
+    .select()
+    .from(users)
+    .where(eq(users.email, email))
+    .get();
+
+  if (existingUser) {
+    throw createError({
+      status: 409,
+      message: "Данный email уже существует",
+    });
+  }
+
+  const hashedPassword = await hashPassword(password);
+
+  const newUser = await db
     .insert(users)
     .values({
-      email: "a.kaipzhanov@gmail.com",
-      password: await hashPassword("almat123"),
+      email: email,
+      password: hashedPassword,
     })
     .returning({
       id: users.id,
       email: users.email,
     })
     .get();
+
+  await setUserSession(event, {
+    user: {
+      id: newUser.id,
+      email: newUser.email,
+    },
+    loggedInAt: Date.now(),
+  });
+
+  return newUser;
 });
